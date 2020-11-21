bbc70fbcdbbcd94b904d0b19726d25aea4bd3793
Johny Wills <jonathan_wills@icloud.com>
2020-11-21 00:08:11 +0000

    started info panel, finished search bar and title look, saving text input with hook

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,57 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	const [ipAddress, setIpAddress] = useState('');
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+	const logIP = () => {
+		console.log(ipAddress);
+	};
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>IP Address Tracker </title>
+				<link rel='icon' href='/favicon-32x32.png' />
+			</Head>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+			<main className={styles.main}>
+				<div className={styles.topPanel}>
+					<div className={styles.title}>IP Address Tracker</div>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+					<div className={styles.searchBar}>
+						<input
+							type='text'
+							className={styles.inputBar}
+							placeholder='Search for any IP address or domain'
+							onChange={(e) => setIpAddress(e.target.value)}
+						/>
+						<button className={styles.searchButton} onClick={() => logIP()} />
+					</div>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+					<div className={styles.infoPanel}>
+						<div className={styles.ip}>
+							<div className={styles.infoTitle}>IP ADDRESS</div>
+							<div className={styles.result}>{ipAddress}</div>
+						</div>
+						<div className={styles.location}>
+							<div className={styles.infoTitle}>LOCATION</div>
+							<div className={styles.result}>Cambridge...</div>
+						</div>
+						<div className={styles.timezone}>
+							<div className={styles.infoTitle}>TIMEZONE</div>
+							<div className={styles.result}>UTC -05:00</div>
+						</div>
+						<div className={styles.isp}>
+							<div className={styles.infoTitle}>ISP</div>
+							<div className={styles.result}>Sky</div>
+						</div>
+					</div>
+				</div>
+				<div className={styles.bottomPanel}></div>
+			</main>
+		</div>
+	);
 }
