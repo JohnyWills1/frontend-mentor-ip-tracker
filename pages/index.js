0bc0cbbe8b4da15785b697dd924e3132884b179a
Johny Wills <jonathan_wills@icloud.com>
2020-11-21 23:38:01 +0000

    added api functionality, added no SSR mapbox map, added mobile styling support

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,48 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
+import dynamic from 'next/dynamic';
 
 export default function Home() {
 	const [ipAddress, setIpAddress] = useState('');
+	const [ip, setIp] = useState('');
+	const [timezone, setTimezone] = useState('');
+	const [isp, setIsp] = useState('');
+	const [location, setLocation] = useState('');
+	const [lat, setLat] = useState(0);
+	const [lng, setLng] = useState(0);
+
+	const DynamicMapNoSSR = dynamic(() => import('../components/Map'), {
+		ssr: false,
+	});
 
 	const logIP = () => {
 		console.log(ipAddress);
+		axios
+			.get(
+				'https://cors-anywhere.herokuapp.com/https://geo.ipify.org/api/v1?apiKey=' +
+					process.env.NEXT_PUBLIC_API_KEY +
+					'&ipAddress=' +
+					ipAddress
+			)
+			.then(({ data }) => {
+				setIp(data.ip);
+				setTimezone(data.location.timezone);
+				setIsp(data.isp);
+				setLocation(
+					data.location.city +
+						', ' +
+						data.location.region +
+						', ' +
+						data.location.country
+				);
+				setLat(data.location.lat);
+				setLng(data.location.lng);
+			})
+			.catch((err) => {
+				console.error(err);
+			});
 	};
 
 	return (
@@ -15,6 +50,10 @@ export default function Home() {
 			<Head>
 				<title>IP Address Tracker </title>
 				<link rel='icon' href='/favicon-32x32.png' />
+				<link
+					href='https://api.mapbox.com/mapbox-gl-js/v0.51.0/mapbox-gl.css'
+					rel='stylesheet'
+				/>
 			</Head>
 
 			<main className={styles.main}>
@@ -32,25 +71,30 @@ export default function Home() {
 					</div>
 
 					<div className={styles.infoPanel}>
-						<div className={styles.ip}>
+						<div className={styles.statPanel}>
 							<div className={styles.infoTitle}>IP ADDRESS</div>
-							<div className={styles.result}>{ipAddress}</div>
+							<div className={styles.result}>{ip}</div>
 						</div>
-						<div className={styles.location}>
+						<div className={styles.divider} />
+						<div className={styles.statPanel}>
 							<div className={styles.infoTitle}>LOCATION</div>
-							<div className={styles.result}>Cambridge...</div>
+							<div className={styles.result}>{location}</div>
 						</div>
-						<div className={styles.timezone}>
+						<div className={styles.divider} />
+						<div className={styles.statPanel}>
 							<div className={styles.infoTitle}>TIMEZONE</div>
-							<div className={styles.result}>UTC -05:00</div>
+							<div className={styles.result}>{timezone}</div>
 						</div>
-						<div className={styles.isp}>
+						<div className={styles.divider} />
+						<div className={styles.statPanel}>
 							<div className={styles.infoTitle}>ISP</div>
-							<div className={styles.result}>Sky</div>
+							<div className={styles.result}>{isp}</div>
 						</div>
 					</div>
 				</div>
-				<div className={styles.bottomPanel}></div>
+				<div className={styles.bottomPanel}>
+					<DynamicMapNoSSR lat={lat} lng={lng} />
+				</div>
 			</main>
 		</div>
 	);
